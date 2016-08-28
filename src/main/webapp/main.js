2e55e466580941a094482e5ac8b0a1c9b94cf871
Alberto Morales <mail@albertomorales.eu>
2016-08-28 18:44:05 +0200

    introduciendo la directiva de servers (ya están también la de project y environment)

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -31,25 +31,49 @@ myModule.controller('mainController', ['$scope', '$http', function($scope, $http
 myModule.directive('projectInfo', [function() {
     return {
       restrict: 'E',
-      template:'<div id="ci-proyecto">' +
-    	          '<div id="ci-proyecto-nombre">{{curProject.description}}</div><p>Environments</p>' +
+      replace: true,
+      template: '<div id="ci-proyecto">' +
+    	          '<div id="ci-proyecto-nombre">{{curProject.description}}</div>' +
+    	          '<p>Environments</p>' +
     	          '<div id="ci-proyecto-entornos" style="border: 1px solid red;" >' +
-    	          	'<div ng-repeat="environment in curProject.environments">' +
+    	          	'<ng-repeat ng-repeat="environment in curProject.environments">' +
     	          		'<environment-info></environment-info>' +
-    	          	'</div>' +
+    	          	'</ng-repeat>' +
     	          '</div>' +
-    	         '</div>'
+    	        '</div>'
     };
   }]);
 
 myModule.directive('environmentInfo', [function() {
     return {
       restrict: 'E',
-      template:'<div style="border: 1px solid blue;"><div><a href="#" ng-click="selectEnvironment(environment)">{{ environment.id }}</a><div>' +
-                  '<div>' +
-                     '<p>Descripción: {{ environment.description }}</p>' +
-                     '<p>URL: {{ environment.homeURL }}&nbsp;<a href="{{ environment.homeURL }}" target="_blank"><span>.</span>ir</a></p>' +
+      replace: true,
+      template: '<div id="ci-entorno-{{environment.id}}" style="border: 1px solid blue;">' +
+      			  '<div id="ci-entorno-id-{{environment.id}}"><a href="#" ng-click="selectEnvironment(environment)">{{ environment.id }}</a></div>' +
+                  '<div id="ci-entorno-datos-{{environment.id}}">' +
+                    '<p>Descripción: {{ environment.description }}</p>' +
+                    '<p>URL: {{ environment.homeURL }}&nbsp;<a href="{{ environment.homeURL }}" target="_blank"><span>.</span>ir</a></p>' +
+                    '<p>Versión: </p>' +
+                    '<p>Conexión: </p>' +
+                    '<p>Esquema: </p>' +
+                    '<div id="ci-entorno-servidores-{{environment.id}}">' +
+                    	'<p>Servers</p>' +
+                    	'<table id="ci-tabla-servidores-{{environment.id}}"><thead><tr><th>Descripción</th><th>IP</th></tr></thead><tbody>' +
+    	          		  	'<tr server-info ng-repeat="server in environment.servers" obj="server" ></tr>' +
+                    	'</tbody></table>' +
+                    '</div>'+
                   '</div>'+
-               '</div>'
+                '</div>'
+    };
+  }]);
+
+myModule.directive('serverInfo', [function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+    	  'server' : '=obj'
+        },
+      template: '<tr id="ci-fila-servidor-{{server.id}}"><td>{{server.description}}</td><td>{{server.address}}</td></tr>'
     };
   }]);
